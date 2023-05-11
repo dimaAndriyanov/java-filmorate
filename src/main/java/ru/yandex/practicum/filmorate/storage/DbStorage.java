0b4b2f92e373ddff9add645df872aa06293bbdea
Dmitriy <d.79052201159@yandex.ru>
2023-05-11 03:45:46 +0300

    style: renamed dao classes, corrected sql queries fix: added abstract superclass for db storage fix: reduced sql queries amount for get methods for users and films

--- src/main/java/ru/yandex/practicum/filmorate/storage/DbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/DbStorage.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.storage;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 
-abstract public class DbStorage {
+public abstract class DbStorage {
     protected final JdbcTemplate jdbcTemplate;
 
     public DbStorage(JdbcTemplate jdbcTemplate) {
